f38dce3e594b47049dcd276333b257b3ee5a844c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-11 20:14:59 +0300

    Added abstract class-prototype for all collections of IMediaItem - MediaSet

---
+++ MediaLibrary/MediaLibrary/Classes/MediaSet.cs
@@ -0,0 +1,41 @@
+﻿using MediaLibrary.Creator;
+using MediaLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace MediaLibrary.Classes
+{
+    public abstract class MediaSet
+    {
+        public string Name { get; set; }
+        public List<IMediaItem> Items { get; protected set; }
+        protected IMediaCreator _itemCreator;
+
+        public void Play()
+        {
+            if (Items.Count != 0)
+            {
+                Console.WriteLine("Playing...");
+
+                foreach (var item in Items)
+                {
+                    item.Play();
+                }
+
+                Console.WriteLine("Playing finished...");
+            }
+            else
+            {
+                Console.WriteLine("Playlist is empty...");
+            }
+        }
+
+        public void ShowPlaylist()
+        {
+            foreach (var item in Items)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
