006b6f18f5c43306d731fa49447ee9575b42f5c4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-11 20:43:28 +0300

    Added class Serial - this class contains only video and image elements

---
+++ MediaLibrary/MediaLibrary/MediaCollections/Serial.cs
@@ -0,0 +1,38 @@
+﻿using MediaLibrary.Classes;
+using MediaLibrary.Creator;
+using MediaLibrary.Interfaces;
+using System.Collections.Generic;
+
+namespace MediaLibrary.MediaCollections
+{
+    //
+    //this class can only contain video and image elements
+    //
+    public class Serial : MediaSet
+    {
+        public Serial(string name)
+        {
+            Name = name;
+            Items = new List<IMediaItem>();
+        }
+
+        public Serial(string name, IList<Video> videos, IList<Image> images)
+        {
+            Items = new List<IMediaItem>();
+            Items.AddRange(videos);
+            Items.AddRange(images);
+        }
+
+        public void AddVideo(string name)
+        {
+            _itemCreator = new VideoCreator();
+            Items.Add(_itemCreator.CreateItem(name));
+        }
+
+        public void AddImage(string name)
+        {
+            _itemCreator = new ImageCreator();
+            Items.Add(_itemCreator.CreateItem(name));
+        }
+    }
+}
