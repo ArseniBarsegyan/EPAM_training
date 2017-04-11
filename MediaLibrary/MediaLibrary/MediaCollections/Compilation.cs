0efb0a0a4ce353ee2ce643efe1186a71064ae2ab
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-11 20:51:42 +0300

    Added Compilation class, an implementation of Disk class with CRUD operations

---
+++ MediaLibrary/MediaLibrary/MediaCollections/Compilation.cs
@@ -0,0 +1,61 @@
+﻿using MediaLibrary.Classes;
+using MediaLibrary.Creator;
+using MediaLibrary.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MediaLibrary.MediaCollections
+{
+    //
+    //this can only contain audio and photo items with ChangeElement functionality
+    //
+    public class Compilation : Disk
+    {
+        public Compilation(string name) : base(name)
+        {
+
+        }
+
+        public Compilation(string name, IList<AudioTrack> audios, IList<Image> images) 
+            : base (name, audios, images)
+        {
+
+        }
+
+        //
+        //Create
+        public void AddAudio(string name)
+        {
+            _itemCreator = new AudioCreator();
+            Items.Add(_itemCreator.CreateItem(name));
+        }
+
+        public void AddImage(string name)
+        {
+            _itemCreator = new ImageCreator();
+            Items.Add(_itemCreator.CreateItem(name));
+        }
+
+        //
+        //Update
+        public void SwapItem(string oldItemName, IMediaItem newItem)
+        {
+            var oldItem = Items.FirstOrDefault(x => x.Name.Equals(oldItemName));
+            Items.Remove(oldItem);
+            Items.Add(newItem);
+        }
+
+        //
+        //Delete
+        public void RemoveItem(string itemName)
+        {
+            var item = Items.FirstOrDefault(x => x.Name.Equals(itemName));
+            Items.Remove(item);
+        }
+
+        public void RemoveAll()
+        {
+            Items = new List<IMediaItem>();
+        }
+    }
+}
