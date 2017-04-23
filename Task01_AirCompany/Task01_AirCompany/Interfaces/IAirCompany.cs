8da64605224f055aad1b63de03437204671be295
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 12:49:31 +0300

    Added IAirCompany interface

---
+++ Task01_AirCompany/Task01_AirCompany/Interfaces/IAirCompany.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Task01_AirCompany.Interfaces
+{
+    public interface IAirCompany
+    {
+        string Name { get; }
+        ICollection<IPlane> Planes { get; }
+
+        void AddPlane(IPlane plane);
+        void RemovePlane(IPlane plane);
+
+        int GetTotalCapacity();
+        int GetTotalCarryingWeight();
+        void SortPlanesByFlyDistance();
+        void GetPlaneByFuelConsumption(int startValue, int endValue);
+    }
+}
