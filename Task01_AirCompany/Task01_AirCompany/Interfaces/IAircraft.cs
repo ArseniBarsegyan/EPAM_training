df6151221da0375c797b2768e8197d46fe568603
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 11:01:45 +0300

    Added IAircraft interface

---
+++ Task01_AirCompany/Task01_AirCompany/Interfaces/IAircraft.cs
@@ -0,0 +1,14 @@
+﻿namespace Task01_AirCompany.Interfaces
+{
+    public interface IAircraft
+    {
+        string Name { get; }
+        int Speed { get; }
+        int FuelConsumption { get; }
+        int FuelCapacity { get; }
+
+        decimal GetFlyDistance();
+        int GetCarryingWeight();
+        void GetInfo();
+    }
+}
