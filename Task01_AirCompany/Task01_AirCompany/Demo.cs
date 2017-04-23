38c972fd9960718acf746590c742f92b744e3a15
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 17:00:47 +0300

    Class Program renamed to Demo. Added demo

---
+++ Task01_AirCompany/Task01_AirCompany/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Task01_AirCompany.Classes;
+using Task01_AirCompany.Interfaces;
+
+namespace Task01_AirCompany
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Creating collection of planes
+            List<IPlane> planes = new List<IPlane>()
+            {
+                new Airbus("Airbus01", 900, 6000, 130000, 12000),
+                new Boeing("Boeing01", 1000, 5000, 120000, 400),
+                new ANSportsPlane("AN01", 300, 300, 600, 2)
+            };
+
+            //Creating new company
+            AirCompany airCompany = new AirCompany("Airlines", planes);
+
+            //Show all planes sorted by fly distance
+            Console.WriteLine("Sorted planes");
+            airCompany.SortPlanesByFlyDistance();
+            Console.WriteLine("--------------------");
+
+            //Show total capacity of the company and total carrying weight
+            Console.WriteLine("Info about company");
+            Console.WriteLine("{0} total capacity: {1}",airCompany.Name, airCompany.GetTotalCapacity());
+            Console.WriteLine("{0} total carrying weight of all planes: {1}", 
+                airCompany.Name, airCompany.GetTotalCarryingWeight());
+            Console.WriteLine("--------------------");
+
+            //Find all planes with fuel consumption between 0 and 1000 l/h
+            Console.WriteLine("Founded planes by fuel consumption");
+            airCompany.FindPlaneByFuelConsumption(0, 1000);
+        }
+    }
+}
