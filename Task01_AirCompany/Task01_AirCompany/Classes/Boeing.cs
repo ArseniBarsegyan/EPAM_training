aaba3d904c63d34ee3664bd6a552dd781656f9e3
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 13:02:38 +0300

    Added Boeing class that inherits from IPassengerPlane. Added auto-properties and constructor

---
+++ Task01_AirCompany/Task01_AirCompany/Classes/Boeing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Task01_AirCompany.Interfaces;
+
+namespace Task01_AirCompany.Classes
+{
+    public class Boeing : IPassengerPlane
+    {
+        public string Name { get; private set; }
+        public int Speed { get; private set; }
+        public int FuelConsumption { get; private set; }
+        public int FuelCapacity { get; private set; }
+        public int PassengersNumber { get; private set; }
+
+        public Boeing(string name, int speed, int fuelConsumption, int fuelCapacity, int passengersNumber)
+        {
+            Name = name;
+            Speed = speed;
+            FuelConsumption = fuelConsumption;
+            FuelCapacity = fuelCapacity;
+            PassengersNumber = passengersNumber;
+        }
+
+        public int GetCarryingWeight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetFlyDistance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
