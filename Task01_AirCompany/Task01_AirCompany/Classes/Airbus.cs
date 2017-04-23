cfccbf0e280e14aef3d8f81bf3e9bb615258d87a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 12:54:41 +0300

    Added Airbus class that inherits ICargoPlane interface. Added auto-properties and constructor of this class

---
+++ Task01_AirCompany/Task01_AirCompany/Classes/Airbus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task01_AirCompany.Interfaces;
+
+namespace Task01_AirCompany.Classes
+{
+    public class Airbus : ICargoPlane
+    {   
+        public string Name { get; private set; }
+        public int Speed { get; private set; }
+        public int FuelConsumption { get; private set; }
+        public int FuelCapacity { get; private set; }
+        public int CarryingCapacity { get; private set; }
+
+        public Airbus(string name, int speed, int fuelConsumption, int fuelCapacity, int carryingCapacity)
+        {
+            Name = name;
+            Speed = speed;
+            FuelConsumption = fuelConsumption;
+            FuelCapacity = fuelCapacity;
+            CarryingCapacity = carryingCapacity;
+        }
+
+        public int GetCarryingWeight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetFlyDistance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
