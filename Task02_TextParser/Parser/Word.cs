a092c71b3cf0bf7bc771e082c05e6a8d40f8639e
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:30:42 +0300

    Added class Word (will be used in concordance). DocumentCreator: removed unnecessary usings

---
+++ Task02_TextParser/Parser/Word.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    //This class will be use in concordance
+    public class Word
+    {
+        public Word(string value, int repeatCount, int numberOfPage)
+        {
+            Value = value;
+            RepeatCount = repeatCount;
+            PagesNumbers = new List<int>();
+            PagesNumbers.Add(numberOfPage);
+        }
+
+        public string Value { get; set; }
+        public int RepeatCount { get; set; }
+        public List<int> PagesNumbers { get; set; }
+    }
+}
