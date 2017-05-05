29d99f2a2d570b2406dc5f470310c47228bdf166
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:04:52 +0300

    Added class "Document" (include list of pages)

---
+++ Task02_TextParser/Parser/Document.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    public class Document
+    {
+        public Document(string name, List<Page> pages)
+        {
+            Name = name;
+            Pages = pages;
+        }
+
+        public string Name { get; private set; }
+        public List<Page> Pages { get; private set; }
+    }
+}
