c2523a888bab0d0666ca456afe99ad9c916ff988
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:11:13 +0300

    Added class "DocumentCreator". this class creates object model from text file. Defined fields abd constructor

---
+++ Task02_TextParser/Parser/DocumentCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    //Creating a document model from text file
+    public class DocumentCreator
+    {
+        private StreamReader _streamReader;
+        private List<Page> _pages;
+        private Document _document;
+        private Page _page;
+        private string _fileName;
+        private int _numberOfLinesPerPage;
+        private string _exceptionMessage;
+
+        public DocumentCreator(string fileName, int numberOfLinesPerPage)
+        {
+            
+        }
+
+    }
+}
