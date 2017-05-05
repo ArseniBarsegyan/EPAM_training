902a67b560e2e6995b3cc3b77358496a5e44db8c
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:02:38 +0300

    Added class "Page"

---
+++ Task02_TextParser/Parser/Page.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    public class Page
+    {
+        public Page(int currentPageNumber, int numberOfLines)
+        {
+            CurrentPageNumber = currentPageNumber;
+            NumberOfLines = numberOfLines;
+            Lines = new List<string>();
+        }
+
+        public int CurrentPageNumber { get; private set; }
+        public int NumberOfLines { get; private set; }
+        public List<string> Lines { get; private set; }
+
+        public void AddLine(string line)
+        {
+            Lines.Add(line);
+        }
+    }
+}
