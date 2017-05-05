99f4a01a7f2c9f3fcb2abf6dbb8a2457c5d04d06
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:44:23 +0300

    Added class TextParser. Defined constructor and fields

---
+++ Task02_TextParser/Parser/TextParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    //Processes document model and
+    //provides list of words
+    public class TextParser
+    {
+        private List<Page> _pages;
+        private List<Word> _words;
+
+        public TextParser(Document document)
+        {
+            _pages = document.Pages;
+        }
+    }
+}
