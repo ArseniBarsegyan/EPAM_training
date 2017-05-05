c8d6e5624f8b4aab480d0efaf0917581c7d997e2
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 12:12:17 +0300

    Demo: Created model of text document

--- Task02_TextParser/Demo/Program.cs
+++ Task02_TextParser/Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Parser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            DocumentCreator creator = new DocumentCreator("D:\\TestDocumentForTask2.doc", 5);
+            Document document = creator.CreateDocumentModel();
         }
     }
 }
