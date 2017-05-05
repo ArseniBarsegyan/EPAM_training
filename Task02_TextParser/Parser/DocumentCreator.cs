5ed5b813b8a11bbfb470af648eb8b120696cd685
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-05 11:18:27 +0300

    DocumentCreator: Added method ReadFileContent(). This method reads file data and fills pages

--- Task02_TextParser/Parser/DocumentCreator.cs
+++ Task02_TextParser/Parser/DocumentCreator.cs
@@ -23,5 +23,40 @@ namespace Parser
             
         }
 
+        //Reading file data and filling pages
+        //Every page can contain only limited number of lines
+        private void ReadFileContent()
+        {
+            _pages = new List<Page>();
+
+            string line;
+            int countLines = 1;
+            int pageNumber = 1;
+            _page = new Page(pageNumber, _numberOfLinesPerPage);
+
+            while((line = _streamReader.ReadLine()) != null)
+            {
+                //When all lines of page are filled adding filled page to list of pages
+                //Creating new page and start filling it again. Counting of lines starting again
+                if (countLines > _numberOfLinesPerPage)
+                {
+                    _pages.Add(_page);
+                    pageNumber++;
+                    _page = new Page(pageNumber, _numberOfLinesPerPage);
+                    countLines = 1;
+                }
+
+                //if page isn't filled continue to filling it and counting lines
+                _page.AddLine(line);
+                countLines++;
+            }
+
+            //Adding last page to page list
+            _pages.Add(_page);
+
+            //Closing stream
+            _streamReader.Close();
+        }
+
     }
 }
