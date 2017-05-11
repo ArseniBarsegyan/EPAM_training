ef3e0e0a02a62a093399b4ce6435c89d7897f5c4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 00:35:18 +0300

    Added class "Document" that contains Collection of Sentences

---
+++ Task02_TextParser/Parser/Document.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    public class Document
+    {
+        public Document(ICollection<Sentence> sentences)
+        {
+            Sentences = sentences;
+        }
+
+        public ICollection<Sentence> Sentences { get; private set; }
+
+    }
+}
