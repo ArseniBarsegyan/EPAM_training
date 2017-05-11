960865e89b1ff9e190ab4d76159c11ab1b07914f
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 00:25:29 +0300

    Added static class TextPaser. Contain extension for string - ParseText. Added private methods that check current string element and return if this element is a Symbol / WordSeparator / SentenceSeparator. Added method that replaces all sequences of whitespace and tabs

---
+++ Task02_TextParser/Parser/TextParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public static class TextParser
+    {
+        public static List<Sentence> ParseText(this string text)
+        {
+            
+        }
+
+        private static void ReplaceAllTabsAndWhiteSpaceSequences(string text)
+        {
+            text = Regex.Replace(text, @"\s+", " ");
+            text = Regex.Replace(text, @"\t+", " ");
+        }
+
+        private static bool IsSymbol(string s)
+        {
+            return new Regex("[A-Za-z0-9]").IsMatch(s);
+        }
+
+        private static bool IsWordSeparator(string s)
+        {
+            return new Regex(@"\s|\:|\-|\'|\,").IsMatch(s);
+        }
+
+        private static bool IsSentenceSeparator(string s)
+        {
+            return new Regex(@"\.|\?|\!").IsMatch(s);
+        }
+    }
+}
