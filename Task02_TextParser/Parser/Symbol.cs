a15e29d06064578e6eb4190d001c1f13d4281e7a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 00:01:16 +0300

    Added class "Symbol". Symbol is a part of Word

---
+++ Task02_TextParser/Parser/Symbol.cs
@@ -0,0 +1,12 @@
+﻿namespace Parser
+{
+    public class Symbol
+    {
+        public Symbol(char value)
+        {
+            Value = value;
+        }
+
+        public char Value { get; private set; }
+    }
+}
