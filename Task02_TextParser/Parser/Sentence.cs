1727ea68a7a6cc7b1e97cd739757b5cc5b6d320a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 00:18:04 +0300

    Added class "Sentence". Contains collection of ISentenceItem

---
+++ Task02_TextParser/Parser/Sentence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parser
+{
+    public class Sentence
+    {
+        public Sentence(ICollection<ISentenceItem> sentenceItems)
+        {
+            SentenceItems = sentenceItems;
+        }
+
+        public ICollection<ISentenceItem> SentenceItems { get; private set; }
+
+        public int GetWordCount()
+        {
+            return SentenceItems.OfType<Word>().Count();
+        }
+
+        public override string ToString()
+        {
+            return SentenceItems.Aggregate(String.Empty, (current, element) => current + element.Value);
+        }
+
+        public bool IsInterrogativeSentence()
+        {
+            return SentenceItems.Any(item => item.Value.Contains("?"));
+        }
+
+        public IEnumerable<Word> GetAllUniqueWords(int length)
+        {
+            return SentenceItems.Where(s => s is Word && s.Value.Length == length)
+                .GroupBy(s => s.Value)
+                .Select(s => (Word) s.First());
+        }
+    }
+}
