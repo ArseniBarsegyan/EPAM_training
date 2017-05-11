8005bcd65f8a25971af1641faa87dd33c717ece8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 00:06:59 +0300

    Added class "Word" - contains collection of Symbol

---
+++ Task02_TextParser/Parser/Word.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Parser
+{
+    public class Word
+    {
+        public Word(ICollection<Symbol> symbols)
+        {
+            Symbols = symbols;
+        }
+
+        public ICollection<Symbol> Symbols { get; private set; }
+
+        public override string ToString()
+        {
+            return Symbols.Aggregate(string.Empty, (current, s) => current + s.Value);
+        }
+
+        public void AddSymbol(char c)
+        {
+            Symbols.Add(new Symbol(c));
+        }
+    }
+}
