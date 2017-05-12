934d06a54023d180c7be58edff4e22d62de6c449
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-12 09:14:26 +0300

    Added "Demo"

---
+++ Task02_TextParser/Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Parser;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "how to test my app? simple.";
+            List<Sentence> sentences = text.ParseText();
+            Document document = new Document(sentences);
+
+            var sentences1 = document.ReplaceAllWordsBySubString(2, "replaced string!");
+            foreach (var sentence in sentences1)
+            {
+                Console.WriteLine(sentence);
+            }
+
+            var textWithoutConsonantWords = document.RemoveAllWordsStartWithConsonant();
+            foreach (var sentence in textWithoutConsonantWords)
+            {
+                Console.WriteLine(sentence);
+            }
+        }
+    }
+}
