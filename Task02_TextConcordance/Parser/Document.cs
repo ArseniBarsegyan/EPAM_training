6b1f7bce655bad7affb703252d2d23369bc66ada
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-14 14:29:03 +0300

    Document: List replaced with ICollection

--- Task02_TextConcordance/Parser/Document.cs
+++ Task02_TextConcordance/Parser/Document.cs
@@ -4,13 +4,13 @@ namespace Parser
 {
     public class Document
     {
-        public Document(string name, List<Page> pages)
+        public Document(string name, ICollection<Page> pages)
         {
             Name = name;
             Pages = pages;
         }
 
         public string Name { get; private set; }
-        public List<Page> Pages { get; private set; }
+        public ICollection<Page> Pages { get; private set; }
     }
 }
