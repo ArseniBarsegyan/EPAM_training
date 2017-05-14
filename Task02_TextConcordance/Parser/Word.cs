495dc162d359c5ecad52b53f88d91fca45ba8cf1
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-14 14:27:39 +0300

    Task02_TextConcordance corrected. Word: List replaced with ICollection, added method IncreaseRepeatCount()

--- Task02_TextConcordance/Parser/Word.cs
+++ Task02_TextConcordance/Parser/Word.cs
@@ -5,16 +5,21 @@ namespace Parser
     //This class will be use in concordance
     public class Word
     {
-        public Word(string value, int repeatCount, int numberOfPage)
+        public Word(string value, int repeatCount, int numberOfPage, ICollection<int> pageNumbers)
         {
             Value = value;
             RepeatCount = repeatCount;
-            PagesNumbers = new List<int>();
+            PagesNumbers = pageNumbers;
             PagesNumbers.Add(numberOfPage);
         }
 
         public string Value { get; private set; }
         public int RepeatCount { get; set; }
-        public List<int> PagesNumbers { get; private set; }
+        public ICollection<int> PagesNumbers { get; }
+
+        public void IncreaseRepeatCount()
+        {
+            RepeatCount++;
+        }
     }
 }
