b3caeb675042b8d32dd7623b1bf196f75db7c9d5
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-14 12:44:19 +0300

    Document renamed to Text.

---
+++ Task02_TextParser/Parser/Text.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Parser
+{
+    public class Text
+    {
+        public Text(ICollection<Sentence> sentences)
+        {
+            Sentences = sentences;
+        }
+
+        public ICollection<Sentence> Sentences { get; private set; }
+
+        public IEnumerable<Sentence> GetSortedSentencesByWordsCount()
+        {
+            return Sentences.OrderBy(s => s.GetWordCount());
+        }
+
+        public IEnumerable<Word> GetUniqueWordsInInterrogativeSentences(int length)
+        {
+            return Sentences.Where(x => x.IsInterrogativeSentence())
+                .SelectMany(x => x.GetAllUniqueWords(length));
+        }
+
+        public void RemoveAllWordsStartWithConsonant(int length)
+        {
+            foreach (var sentence in Sentences)
+            {
+                sentence.ReplaceAllSentenceWordsStartWithConsonant(length);
+            }
+        }
+
+        public void ReplaceAllWordsInSentenceBySubString(int sentenceNumber, int length,
+            IEnumerable<ISentenceItem> items)
+        {
+            items = items.Reverse();
+            Sentences.ElementAt(sentenceNumber).ReplaceAllWordsByLengthWithSubString(length, items);
+        }
+    }
+}
