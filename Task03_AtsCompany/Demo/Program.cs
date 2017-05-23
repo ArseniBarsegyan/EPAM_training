49b3eb7643d18fb30bf44d581f600e3e196f9524
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-24 00:02:33 +0300

    Demo: added demo, shows that terminal get into Server's EnablePorts when terminal is on and by default get into Server's DisablePorts

--- Task03_AtsCompany/Demo/Program.cs
+++ Task03_AtsCompany/Demo/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using AtsCompany.Classes;
 
 namespace Demo
 {
@@ -10,6 +7,10 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            var server = new AtsServer("MTS", new List<Port>());
+            var terminal = server.CreateTerminal();
+            terminal.TurnOnTerminal();
+            var terminal2 = server.CreateTerminal();
         }
     }
 }
