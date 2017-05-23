5c132b1dc1fdbe87136a0efe2593e67f32883965
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:59:21 +0300

    AtsServer: Subscribe on all Port events. added methods that handle Port enable and disable events

--- Task03_AtsCompany/AtsCompany/Classes/AtsServer.cs
+++ Task03_AtsCompany/AtsCompany/Classes/AtsServer.cs
@@ -34,13 +34,50 @@ namespace AtsCompany.Classes
             var terminal = new Terminal(number, this);
             var port = new Port(number, terminal, this);
             DisabledPorts.Add(port);
-
+            SubscribeOnAllPortEvents(port);
             return terminal;
+        }
+
+        private void PortOnPortEnabled(object sender)
+        {
+            var port = sender as Port;
+            if (DisabledPorts.Contains(port))
+            {
+                DisabledPorts.Remove(port);
+                EnabledPorts.Add(port);
+            }
+            else if (ActivePorts.ContainsKey(port))
+            {
+                ActivePorts.Remove(port);
+                EnabledPorts.Add(port);
+            }
+        }
+
+        private void PortOnPortDisabled(object sender)
+        {
+            var port = sender as Port;
+
+            if (EnabledPorts.Contains(port))
+            {
+                EnabledPorts.Remove(port);
+                DisabledPorts.Add(port);
+            }
+            else if (ActivePorts.ContainsKey(port))
+            {
+                ActivePorts.Remove(port);
+                DisabledPorts.Add(port);
+            }
         }
 
         private bool IsPortListContainPortByNumber(int number)
         {
             return DisabledPorts.Any(x => x.Number == number);
+        }
+
+        private void SubscribeOnAllPortEvents(Port port)
+        {
+            port.PortEnabled += PortOnPortEnabled;
+            port.PortDisabled += PortOnPortDisabled;
         }
     }
 }
