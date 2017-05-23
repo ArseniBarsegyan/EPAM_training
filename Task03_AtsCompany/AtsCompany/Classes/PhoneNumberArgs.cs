1de302d4e5a661a51de9cb0d930772d96830c4f8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-24 00:04:43 +0300

    Added class PhoneNumberArgs.

---
+++ Task03_AtsCompany/AtsCompany/Classes/PhoneNumberArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AtsCompany.Classes
+{
+    public class PhoneNumberArgs : EventArgs
+    {
+        public readonly int number;
+
+        public PhoneNumberArgs(int number)
+        {
+            this.number = number;
+        }
+    }
+}
