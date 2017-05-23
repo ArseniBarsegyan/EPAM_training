34af9718148ad474f434382ea4b2dbef07160066
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:48:05 +0300

    Added class AtsServer

---
+++ Task03_AtsCompany/AtsCompany/Classes/AtsServer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AtsCompany.Classes
+{
+    public class AtsServer
+    {
+        public AtsServer(string name, ICollection<Port> disabledPorts)
+        {
+            Name = name;
+            DisabledPorts = disabledPorts;
+            ActivePorts = new Dictionary<Port, int>();
+            EnabledPorts = new List<Port>();
+            CallingPorts = new List<Port>();
+        }
+
+        public string Name { get; }
+        public IDictionary<Port, int> ActivePorts { get; }
+        public ICollection<Port> DisabledPorts { get; }
+        public ICollection<Port> EnabledPorts { get; }
+        public ICollection<Port> CallingPorts { get; }
+    }
+}
