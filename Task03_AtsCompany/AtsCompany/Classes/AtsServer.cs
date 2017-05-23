80728785365b9342a0e837624b89cf8d5c799a42
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:49:53 +0300

    AtsServer: added CreateTerminal() method

--- Task03_AtsCompany/AtsCompany/Classes/AtsServer.cs
+++ Task03_AtsCompany/AtsCompany/Classes/AtsServer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AtsCompany.Classes
 {
@@ -18,5 +20,27 @@ namespace AtsCompany.Classes
         public ICollection<Port> DisabledPorts { get; }
         public ICollection<Port> EnabledPorts { get; }
         public ICollection<Port> CallingPorts { get; }
+
+        //When terminal created it's port get into DisabledPorts list
+        public Terminal CreateTerminal()
+        {
+            var randomNumber = new Random();
+            var number = randomNumber.Next(111111, 999999);
+
+            while (IsPortListContainPortByNumber(number))
+            {
+                number = randomNumber.Next(111111, 999999);
+            }
+            var terminal = new Terminal(number, this);
+            var port = new Port(number, terminal, this);
+            DisabledPorts.Add(port);
+
+            return terminal;
+        }
+
+        private bool IsPortListContainPortByNumber(int number)
+        {
+            return DisabledPorts.Any(x => x.Number == number);
+        }
     }
 }
