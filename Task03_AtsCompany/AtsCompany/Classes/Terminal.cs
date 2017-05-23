9049a3b2fdab655a0a9930f7da00f437b4268029
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:45:01 +0300

    Added class Terminal

---
+++ Task03_AtsCompany/AtsCompany/Classes/Terminal.cs
@@ -0,0 +1,14 @@
+﻿namespace AtsCompany.Classes
+{
+    public class Terminal
+    {
+        public Terminal(int number, AtsServer server)
+        {
+            Number = number;
+            Server = server;
+        }
+
+        public AtsServer Server { get; private set; }
+        public int Number { get; private set; }
+    }
+}
