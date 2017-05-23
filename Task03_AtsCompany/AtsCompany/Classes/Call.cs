f0920b11a15ade349114fe87f4f5cc314c88be6c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:42:33 +0300

    Added class Call

---
+++ Task03_AtsCompany/AtsCompany/Classes/Call.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AtsCompany.Classes
+{
+    class Call
+    {
+        public Call()
+        {
+            StartTime = DateTime.Now;
+        }
+
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public int SenderNumber { get; }
+        public int RecieverNumber { get; }
+
+        public void FinishCall()
+        {
+            EndTime = DateTime.Now;
+            Duration = EndTime - StartTime;
+        }
+    }
+}
