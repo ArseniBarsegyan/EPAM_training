a6359fae61417c8c4b4e680435a820b5811c2f7d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:46:24 +0300

    Added class Port

---
+++ Task03_AtsCompany/AtsCompany/Classes/Port.cs
@@ -0,0 +1,18 @@
+﻿namespace AtsCompany.Classes
+{
+    public class Port
+    {
+        public Port(int number, Terminal terminal, AtsServer server)
+        {
+            Number = number;
+            State = PortState.Disabled;
+            Terminal = terminal;
+            Server = server;
+        }
+
+        public Terminal Terminal { get; }
+        public int Number { get; }
+        public PortState State { get; private set; }
+        public AtsServer Server { get; private set; }
+    }
+}
