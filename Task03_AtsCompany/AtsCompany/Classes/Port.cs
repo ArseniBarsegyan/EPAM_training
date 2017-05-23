b331e337536cd4a18bb994cbb2b78d2bcb67051d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-23 23:56:52 +0300

    Port: Subscribe on Terminal events (method SubscribeOnAllTerminalEvents()) After event happens in Terminal Port will change it state and invoke next event that will get to server

--- Task03_AtsCompany/AtsCompany/Classes/Port.cs
+++ Task03_AtsCompany/AtsCompany/Classes/Port.cs
@@ -8,11 +8,48 @@
             State = PortState.Disabled;
             Terminal = terminal;
             Server = server;
+            SubscribeOnAllTerminalEvents();
         }
 
         public Terminal Terminal { get; }
         public int Number { get; }
         public PortState State { get; private set; }
         public AtsServer Server { get; private set; }
+
+        private void TerminalOnTerminalIsEnabled(object sender)
+        {
+            State = PortState.Enabled;
+            OnPortEnabled();
+        }
+
+        public delegate void PortEnabledHandler(object sender);
+
+        public event PortEnabledHandler PortEnabled;
+
+        protected virtual void OnPortEnabled()
+        {
+            PortEnabled?.Invoke(this);
+        }
+
+        private void TerminalOnTerminalIsDisabled(object sender)
+        {
+            State = PortState.Disabled;
+            OnPortDisabled();
+        }
+
+        public delegate void PortDisabledHandler(object sender);
+
+        public event PortDisabledHandler PortDisabled;
+
+        protected virtual void OnPortDisabled()
+        {
+            PortDisabled?.Invoke(this);
+        }
+
+        private void SubscribeOnAllTerminalEvents()
+        {
+            Terminal.TerminalIsEnabled += TerminalOnTerminalIsEnabled;
+            Terminal.TerminalIsDisabled += TerminalOnTerminalIsDisabled;
+        }
     }
 }
