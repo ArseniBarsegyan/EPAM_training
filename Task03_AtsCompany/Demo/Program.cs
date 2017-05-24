3e09e6d1540dc8243ed1c5ffa4076bce432f697c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-24 23:08:00 +0300

    Added full demo

--- Task03_AtsCompany/Demo/Program.cs
+++ Task03_AtsCompany/Demo/Program.cs
@@ -8,11 +8,18 @@ namespace Demo
         static void Main(string[] args)
         {
             var server = new AtsServer("MTS", new List<Port>());
-            var terminal = server.CreateTerminal();
-            terminal.TurnOnTerminal();
-            var terminal2 = server.CreateTerminal();
-            terminal.MakeCall(111);
-            terminal2.MakeCall(terminal.Number);
+            var port1 = server.CreatePort();
+            var terminal1 = new Terminal(port1);
+            port1.SetCurrentTerminal(terminal1);
+            terminal1.TurnOnTerminal();
+            terminal1.TurnOffTerminal();
+            terminal1.MakeCall(123);
+
+            var port2 = server.CreatePort();
+            var terminal2 = new Terminal(port2);
+            port2.SetCurrentTerminal(terminal2);
+            terminal2.TurnOnTerminal();
+            terminal1.MakeCall(port2.Number);
         }
     }
 }
