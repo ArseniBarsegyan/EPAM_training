d67f318816bd55db5cf11db50fd244a88a49acef
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 11:39:33 +0300

    AtsManager: added properties and methods CreateUserAccount() and CreateTerminalForUser()

--- Task03_AtsCompany/BillingSystem/AtsManager.cs
+++ Task03_AtsCompany/BillingSystem/AtsManager.cs
@@ -1,6 +1,34 @@
-﻿namespace BillingSystem
+﻿using System.Collections.Generic;
+using AtsCompany.Classes;
+
+namespace BillingSystem
 {
     public class AtsManager
     {
+        public AtsManager(string name, AtsServer server)
+        {
+            Name = name;
+            Server = server;
+            UsersAccounts = new List<UserAccount>();
+        }
+
+        public string Name { get; }
+        public AtsServer Server { get; }
+        public ICollection<UserAccount> UsersAccounts { get; }
+
+        public UserAccount CreateUserAccount(string name, IRate rate)
+        {
+            var user = new UserAccount(name, rate, new List<Terminal>(), this);
+            UsersAccounts.Add(user);
+            return user;
+        }
+
+        public void CreateTerminalForUser(UserAccount userAccount)
+        {
+            var port = Server.CreatePort();
+            var terminal = new Terminal(port);
+            port.SetCurrentTerminal(terminal);
+            userAccount.Terminals.Add(terminal);
+        }
     }
 }
