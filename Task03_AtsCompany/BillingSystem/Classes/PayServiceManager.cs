403664f08a72a10bd181c84c497fa50a7ed9e570
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 21:27:24 +0300

    BillingSystem: added class PayServiceManager (generates information about user calls, count calls price, counts total price for all user calls). Added demo.

---
+++ Task03_AtsCompany/BillingSystem/Classes/PayServiceManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AtsCompany.Classes;
+
+namespace BillingSystem.Classes
+{
+    //This class stores information about every user call (rate, time etc.)
+    public class PayServiceManager
+    {
+        public IDictionary<UserAccount, List<CallInfo>> UsersCallsInfoDictionary { get; private set; }
+
+        public PayServiceManager(AtsManager manager, AtsServer server)
+        {
+            Manager = manager;
+            Server = server;
+            server.CallFinished += ServerOnCallFinished;
+            UsersCallsInfoDictionary = new Dictionary<UserAccount, List<CallInfo>>();
+        }
+        
+        private void ServerOnCallFinished(Call call)
+        {
+            foreach (var userAccount in Manager.UsersAccounts)
+            {
+                if (!UsersCallsInfoDictionary.ContainsKey(userAccount))
+                {
+                    UsersCallsInfoDictionary.Add(userAccount, new List<CallInfo>());
+                }
+                
+                foreach (var terminal in userAccount.Terminals)
+                {
+                    if (call.SenderNumber == terminal.Number)
+                    {
+                        var callInfo = new CallInfo(call, userAccount.CurrentRate, true);
+                        callInfo.SetPrice(CountCallPrice(userAccount, callInfo));
+                        UsersCallsInfoDictionary[userAccount].Add(callInfo);
+                    }
+                    else if (call.RecieverNumber == terminal.Number)
+                    {
+                        var callInfo = new CallInfo(call, userAccount.CurrentRate, false);
+                        UsersCallsInfoDictionary[userAccount].Add(callInfo);
+                    }
+                }
+            }
+            CountTotalPayForEveryUser();
+        }
+       
+        //Count call price and reduce user's free minutes
+        private int CountCallPrice(UserAccount userAccount, CallInfo callInfo)
+        {
+            var callPrice = 0;
+            var callDuration = Math.Ceiling(callInfo.Duration.TotalMinutes);
+
+            for (int i = 0; i < callDuration; i++)
+            {
+                if (userAccount.FreeMinutes > 0)
+                {
+                    userAccount.SpendFreeMinute();
+                }
+                else
+                {
+                    callPrice += callInfo.Rate.OneMinutePrice;
+                }
+            }
+            return callPrice;
+        }
+
+        public delegate void MonthPayHandler(IDictionary<UserAccount, double> totalMonthPays);
+        public event MonthPayHandler SendTotalMonthPays;
+
+        //return dictionary with key = UserAccount and value = totalPrice for all calls
+        private void CountTotalPayForEveryUser()
+        {
+            IDictionary<UserAccount, double> totalMonthPays = new Dictionary<UserAccount, double>();
+            foreach (var pair in UsersCallsInfoDictionary)
+            {
+                var userAccount = pair.Key;
+                var userCalls = pair.Value;
+                var totalPay = userCalls.Sum(x => x.Price);
+
+                if (!totalMonthPays.ContainsKey(userAccount))
+                {
+                    totalMonthPays.Add(pair.Key, totalPay);
+                }
+                totalMonthPays[userAccount] = totalPay;
+            }
+            SendTotalMonthPays?.Invoke(totalMonthPays);
+        }
+
+        public AtsManager Manager { get; }
+        public AtsServer Server { get; }
+    }
+}
--- Task03_AtsCompany/Demo/Program.cs
+++ Task03_AtsCompany/Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using AtsCompany.Classes;
+using BillingSystem.Classes;
 
 namespace Demo
 {
@@ -9,20 +11,40 @@ namespace Demo
         static void Main(string[] args)
         {
             var server = new AtsServer("MTS", new List<Port>());
-            var port1 = server.CreatePort();
-            var terminal1 = new Terminal(port1);
-            port1.SetCurrentTerminal(terminal1);
-            terminal1.TurnOnTerminal();
-            terminal1.TurnOffTerminal();
-            terminal1.MakeCall(123);
 
-            var port2 = server.CreatePort();
-            var terminal2 = new Terminal(port2);
-            port2.SetCurrentTerminal(terminal2);
-            terminal2.TurnOnTerminal();
-            terminal1.MakeCall(port2.Number);
+            //var port1 = server.CreatePort();
+            //var terminal1 = new Terminal(port1);
+            //port1.SetCurrentTerminal(terminal1);
+            //terminal1.TurnOnTerminal();
+            //terminal1.TurnOffTerminal();
+            //terminal1.MakeCall(123);
+
+            //var port2 = server.CreatePort();
+            //var terminal2 = new Terminal(port2);
+            //port2.SetCurrentTerminal(terminal2);
+            //terminal2.TurnOnTerminal();
+            //terminal1.MakeCall(port2.Number);
+            //Thread.Sleep(1000);
+            //terminal2.EndCall();
+
+            var manager = new AtsManager("MTS manager system", server);
+            var payService = new PayServiceManager(manager, server);
+
+            var user = manager.CreateUserAccount("Arseni", new SmartRate("Smart", 1, 1));
+            manager.CreateTerminalForUser(user);
+            var user2 = manager.CreateUserAccount("Arseni-2", new SmartRate("Smart-2", 2, 60));
+            manager.CreateTerminalForUser(user2);
+            
+            user2.Terminals.ElementAt(0).TurnOnTerminal();
+            //Making few calls
+            user.MakeCall(user.Terminals.ElementAt(0), user2.Terminals.ElementAt(0).Number);
             Thread.Sleep(1000);
-            terminal2.EndCall();
+            user.EndCall(user.Terminals.ElementAt(0));
+
+            user.MakeCall(user.Terminals.ElementAt(0), user2.Terminals.ElementAt(0).Number);
+            user.EndCall(user.Terminals.ElementAt(0));
+
+            //Test PayServiceManager
         }
     }
 }
