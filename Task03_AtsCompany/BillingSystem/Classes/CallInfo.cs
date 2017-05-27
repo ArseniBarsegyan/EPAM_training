d0be62b0c057333ede4b6237c7c6aebbda4d573a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 19:20:07 +0300

    BillingSystem: added class CallInfo - represents information about Call - have the same properties and in additional, Price, based on CurrentRate

---
+++ Task03_AtsCompany/BillingSystem/Classes/CallInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AtsCompany.Classes;
+using BillingSystem.Interfaces;
+
+namespace BillingSystem.Classes
+{
+    public class CallInfo
+    {
+        public CallInfo(Call call, IRate rate, bool isOutComingCall)
+        {
+            IsOutComingCall = isOutComingCall;
+            StartTime = call.StartTime;
+            EndTime = call.EndTime;
+            Duration = call.Duration;
+            SenderNumber = call.SenderNumber;
+            RecieverNumber = call.RecieverNumber;
+            Rate = rate;
+            Price = 0;
+        }
+
+        public void SetPrice(int price)
+        {
+            Price = price;
+        }
+
+        public IRate Rate { get; }
+        public bool IsOutComingCall { get; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public int SenderNumber { get; private set; }
+        public int RecieverNumber { get; private set; }
+        public int Price { get; private set; }
+    }
+}
