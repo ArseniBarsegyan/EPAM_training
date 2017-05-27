9b3915bc89e5d9480ae002784114e2d2d2443491
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 19:14:10 +0300

    IRate removed to "BillingSystem\Interfaces"

---
+++ Task03_AtsCompany/BillingSystem/Interfaces/IRate.cs
@@ -0,0 +1,9 @@
+﻿namespace BillingSystem.Interfaces
+{
+    public interface IRate
+    {
+        string Name { get; }
+        int OneMinutePrice { get; }
+        int FreeMinutes { get; }
+    }
+}
