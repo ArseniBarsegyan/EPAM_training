fe6f040515eb83c53cc998769b2745bdd0c50a31
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 11:27:41 +0300

    UserAccount: added constructor and properties

--- Task03_AtsCompany/BillingSystem/UserAccount.cs
+++ Task03_AtsCompany/BillingSystem/UserAccount.cs
@@ -1,7 +1,29 @@
-﻿namespace BillingSystem
+﻿using System;
+using System.Collections.Generic;
+using AtsCompany.Classes;
+
+namespace BillingSystem
 {
     public class UserAccount
     {
-        
+        public UserAccount(string name, IRate currentRate, ICollection<Terminal> terminals, AtsManager manager)
+        {
+            Name = name;
+            CurrentRate = currentRate;
+            PreviousRate = CurrentRate;
+            Terminals = terminals;
+            RegistrationTime = DateTime.Now;
+            ChangeRateTime = RegistrationTime;
+            Manager = manager;
+        }
+
+        private double Balance { get; set; }
+        public DateTime ChangeRateTime { get; private set; }
+        public DateTime RegistrationTime { get; private set; }
+        public string Name { get; }
+        public IRate PreviousRate { get; private set; }
+        public IRate CurrentRate { get; private set; }
+        public ICollection<Terminal> Terminals { get; }
+        public AtsManager Manager { get; private set; }
     }
 }
