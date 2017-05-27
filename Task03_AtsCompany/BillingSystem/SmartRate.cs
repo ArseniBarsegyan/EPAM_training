8e805155dc69f7dafc6c8b12d2ffdaaf9814407c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-27 11:33:18 +0300

    BillingSystem: added SmartRate - implementation of IRate

---
+++ Task03_AtsCompany/BillingSystem/SmartRate.cs
@@ -0,0 +1,16 @@
+﻿namespace BillingSystem
+{
+    class SmartRate : IRate
+    {
+        public SmartRate(string name, int oneMinutePrice, int freeMinutes)
+        {
+            Name = name;
+            OneMinutePrice = oneMinutePrice;
+            FreeMinutes = freeMinutes;
+        }
+
+        public string Name { get; }
+        public int OneMinutePrice { get; }
+        public int FreeMinutes { get; }
+    }
+}
