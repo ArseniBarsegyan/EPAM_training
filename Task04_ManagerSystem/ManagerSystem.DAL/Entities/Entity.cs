c2cba8f1e8881b7f0fb3604b39d06b906aa3ab2d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-08 23:47:29 +0300

    DAL: added classes: Entity, Manager, Product

---
+++ Task04_ManagerSystem/ManagerSystem.DAL/Entities/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
