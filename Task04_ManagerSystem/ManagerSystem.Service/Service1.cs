50b5b2284035ea5776e41c46bb7449e4dec588db
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-09 11:35:06 +0300

    Service: updated Service1.cs: now Service can watch Server directory for events and log them

--- Task04_ManagerSystem/ManagerSystem.Service/Service1.cs
+++ Task04_ManagerSystem/ManagerSystem.Service/Service1.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
+﻿using System.ServiceProcess;
 using System.Threading.Tasks;
+using ManagerSystem.Service.Classes;
+using System.Threading;
 
 namespace ManagerSystem.Service
 {
     public partial class Service1 : ServiceBase
     {
+        private Logger _logger;
+
         public Service1()
         {
             InitializeComponent();
+            CanStop = true;
+            CanPauseAndContinue = true;
+            AutoLog = true;
         }
 
         protected override void OnStart(string[] args)
         {
+            _logger = new Logger();
+            var loggerTask = new Task(_logger.Start);
+            loggerTask.Start();
         }
 
         protected override void OnStop()
         {
+            _logger.Stop();
+            Thread.Sleep(1000);
         }
     }
 }
