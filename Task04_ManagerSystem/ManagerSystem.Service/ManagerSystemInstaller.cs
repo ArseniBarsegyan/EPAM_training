a5c1513f80130069c74ffe9da2458869ed0028d4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-10 22:44:04 +0300

    ManagerSystem.Service: added Installer class "ManagerSystemInstaller"

---
+++ Task04_ManagerSystem/ManagerSystem.Service/ManagerSystemInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace ManagerSystem.Service
+{
+    [RunInstaller(true)]
+    public partial class ManagerSystemInstaller : System.Configuration.Install.Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public ManagerSystemInstaller()
+        {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "ManagerService";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
