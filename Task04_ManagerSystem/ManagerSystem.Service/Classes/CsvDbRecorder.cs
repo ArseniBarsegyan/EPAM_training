c7dd351a28cc130ad8fdaa2d07a46d0f76581fcf
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-10 11:49:13 +0300

    Service: added class CsvDbRecorder: added private fields and constructor

---
+++ Task04_ManagerSystem/ManagerSystem.Service/Classes/CsvDbRecorder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerSystem.DAL.Entities;
+using Task04.DAL.Repositories;
+
+namespace ManagerSystem.Service.Classes
+{
+    public class CsvDbRecorder
+    {
+        private string _nameOrConnectionString;
+        private object _lockObject = new object();
+        private GenericRepository<Manager> _managerRepo;
+
+        public CsvDbRecorder(string nameOrConnectionString)
+        {
+            _nameOrConnectionString = nameOrConnectionString;
+        }
+    }
+}
