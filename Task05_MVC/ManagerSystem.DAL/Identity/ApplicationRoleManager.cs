f350a1a4af2e426748543e7123c663b7cf3e0deb
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 23:31:58 +0300

    DAL: Added ApplicationRoleManager (implementation of RoleManager<ApplicationRole>)

---
+++ Task05_MVC/ManagerSystem.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using ManagerSystem.DAL.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace ManagerSystem.DAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, string> store)
+            : base(store)
+        {
+
+        }
+    }
+}
