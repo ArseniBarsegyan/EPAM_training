cfe3166fd9debdf6d557366206ee3f734906bacd
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 23:38:19 +0300

    DAL: added ApplicationContext

---
+++ Task05_MVC/ManagerSystem.DAL/EF/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using ManagerSystem.DAL.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ManagerSystem.DAL.EF
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        static ApplicationContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationContext>());
+        }
+
+        public ApplicationContext(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
