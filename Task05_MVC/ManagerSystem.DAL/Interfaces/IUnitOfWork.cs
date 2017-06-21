c3fc8b56c244af1176772a5e4e8d75652dfc9be4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 23:43:11 +0300

    DAL: Interfaces: added interface IUnitOfWork : IDisposable

---
+++ Task05_MVC/ManagerSystem.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using ManagerSystem.DAL.Entities;
+using ManagerSystem.DAL.Identity;
+using ManagerSystem.DAL.Repositories;
+
+namespace ManagerSystem.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ApplicationUserManager UserManager { get; }
+        ApplicationRoleManager RoleManager { get; }
+        GenericRepository<Client> ClientRepository { get; }
+        GenericRepository<Product> ProductRepository { get; }
+        GenericRepository<Manager> ManagerRepository { get; }
+        GenericRepository<Order> OrderRepository { get; }
+        Task SaveAsync();
+        void Save();
+    }
+}
