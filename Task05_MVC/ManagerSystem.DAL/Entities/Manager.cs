67fccbfbc22afb1efb277a13da74f5cfc1ea8a5f
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 23:36:38 +0300

    DAL: Entities: added Manager

---
+++ Task05_MVC/ManagerSystem.DAL/Entities/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ManagerSystem.DAL.Entities
+{
+    public class Manager : Entity
+    {
+        public Manager()
+        {
+            Orders = new List<Order>();
+        }
+
+        public string LastName { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
