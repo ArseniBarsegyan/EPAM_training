b645046c7bd1aff6a21f27196540abee2ec69c88
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:32:58 +0300

    WebUI: Controllers: added AccountController

---
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerSystem.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+    }
+}
