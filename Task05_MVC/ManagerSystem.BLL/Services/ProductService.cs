7d3dc2a02d070dbd6e9c74bbe52af49efa704872
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:03:16 +0300

    BLL: Services: added ProductService

---
+++ Task05_MVC/ManagerSystem.BLL/Services/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.DAL.Repositories;
+
+namespace ManagerSystem.BLL.Services
+{
+    public class ProductService : IProductService
+    {
+        private UnitOfWork UnitOfWork { get; }
+
+        public ProductService(UnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
