63dab629ad920c07ad42fb09a23f9948b480704e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 11:45:16 +0300

    BLL: Interfaces: added IServiceCreator. Maybe later will replace it with Ninject DI

---
+++ Task05_MVC/ManagerSystem.BLL/Interfaces/IServiceCreator.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagerSystem.BLL.Interfaces
+{
+    public interface IServiceCreator
+    {
+        IUserService CreateUserService(string connectionString);
+        IOrderService CreateOrderService(string connectionString);
+        IManagerService CreateManagerService(string connectionString);
+        IProductService CreateProductService(string connectionString);
+    }
+}
