3b931b3a5343af4c9b730fcda1d5210d68bc6939
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 11:42:26 +0300

    BLL: Interfaces: added IOrderService

---
+++ Task05_MVC/ManagerSystem.BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ManagerSystem.BLL.DTO;
+using ManagerSystem.BLL.Infrastructure;
+
+namespace ManagerSystem.BLL.Interfaces
+{
+    public interface IOrderService
+    {
+        IEnumerable<OrderDto> GetAllOrderList();
+        OrderDto GetOrderDtoById(int id);
+        OperationDetails Create(OrderDto orderDto);
+        OperationDetails Edit(OrderDto orderDto);
+        OperationDetails Delete(int id);
+    }
+}
