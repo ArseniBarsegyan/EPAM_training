548cef6389092bc5d8ea3e7733731491dd206a8e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 13:03:00 +0300

    WebUI: OrderController: added field _orderService added Index method (returns view of all orders from DB)

--- Task05_MVC/ManagerSystem.WebUI/Controllers/OrderController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/OrderController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.BLL.Services;
+using ManagerSystem.DAL.Repositories;
 
 namespace ManagerSystem.WebUI.Controllers
 {
     [Authorize(Roles = "admin")]
     public class OrderController : Controller
     {
+        private IOrderService _orderService = new OrderService(new UnitOfWork("DefaultConnection"));
+
+        public ActionResult Index()
+        {
+            var allOrders = _orderService.GetAllOrderList();
+            return View(allOrders);
+        }
     }
 }
