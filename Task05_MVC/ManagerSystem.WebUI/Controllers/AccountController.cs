aa601c47a8881300ec934d037fbfcb5caefd4329
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:36:03 +0300

    WebUI: AccountController: added methods Login (Get and Post)

--- Task05_MVC/ManagerSystem.WebUI/Controllers/AccountController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using System.Web;
+﻿using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using ManagerSystem.BLL.DTO;
 using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.WebUI.Models;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 
@@ -10,5 +13,37 @@ namespace ManagerSystem.WebUI.Controllers
     {
         private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
         private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDto { Name = model.Name, Password = model.Password };
+                var claim = await UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Incorrect login or password");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
     }
 }
