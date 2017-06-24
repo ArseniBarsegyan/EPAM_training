2c7708b6702139c9d99f522e83b7407e104425ab
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 11:38:37 +0300

    BLL: added OrderDto

---
+++ Task05_MVC/ManagerSystem.BLL/DTO/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ManagerSystem.BLL.DTO
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string ManagerName { get; set; }
+        public DateTime Date { get; set; }
+        public string ClientName { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
