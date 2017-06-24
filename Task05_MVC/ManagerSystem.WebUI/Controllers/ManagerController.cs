2059bc0097a4427513e972f101a6e6197530ebd2
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 13:16:44 +0300

    WebUI: Controllers: added ManagerController. Added field _managerService and method Index(), that returns view of all managers

---
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/ManagerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.BLL.Services;
+using ManagerSystem.DAL.Repositories;
+
+namespace ManagerSystem.WebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class ManagerController : Controller
+    {
+        private IManagerService _managerService = new ManagerService(new UnitOfWork("DefaultConnection"));
+
+        public ActionResult Index()
+        {
+            var allManagers = _managerService.GetAllManagersList();
+            return View(allManagers);
+        }
+    }
+}
