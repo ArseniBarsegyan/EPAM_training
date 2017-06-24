4708491be08d26d15d5eac4a3f6970c2bcc70c4b
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 13:07:45 +0300

    WebUI: OrderController: added Create method (get and post) versions - allow admin create new orders

--- Task05_MVC/ManagerSystem.WebUI/Controllers/OrderController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/OrderController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ManagerSystem.BLL.DTO;
 using ManagerSystem.BLL.Interfaces;
 using ManagerSystem.BLL.Services;
 using ManagerSystem.DAL.Repositories;
+using ManagerSystem.WebUI.Models;
 
 namespace ManagerSystem.WebUI.Controllers
 {
@@ -18,6 +20,33 @@ namespace ManagerSystem.WebUI.Controllers
         {
             var allOrders = _orderService.GetAllOrderList();
             return View(allOrders);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(OrderCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var orderDto = new OrderDto
+                {
+                    ClientName = model.ClientName,
+                    ManagerName = model.ManagerName,
+                    Price = model.Price,
+                    ProductName = model.ProductName,
+                    Date = model.Date
+                };
+
+                var operationDetails = _orderService.Create(orderDto);
+                if (operationDetails.Succedeed)
+                    return RedirectToAction("Index", "Order");
+                ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View(model);
         }
     }
 }
