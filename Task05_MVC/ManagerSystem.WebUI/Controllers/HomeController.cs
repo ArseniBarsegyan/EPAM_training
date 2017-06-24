50c3123bd0e8ff162fb6efd1cccb83923eafe16d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 14:32:03 +0300

    WebUI: HomeController: changed Index(). Now this method will return page with filters by: managerName, productName, date, by price (fromValue, toValue), and divide this info into pages

--- Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
@@ -1,10 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.BLL.Services;
+using ManagerSystem.DAL.Repositories;
 
 namespace ManagerSystem.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IOrderService _orderService = new OrderService(new UnitOfWork("DefaultConnection"));
+        private int PageSize = Convert.ToInt32(ConfigurationManager.AppSettings["PageSize"]);
+
+        public ActionResult Index(string manager, string product, string date, decimal? fromValue
+            , decimal? toValue, int page = 1)
         {
             return View();
         }
