76a26812f4f000cedfa7e7dcd59522c2758621e4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:48:05 +0300

    WebUI: AdminController: added fields UserService and AuthenticationManager (get from OWIN context)

--- Task05_MVC/ManagerSystem.WebUI/Controllers/AdminController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AdminController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace ManagerSystem.WebUI.Controllers
 {
     [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
-        
+        private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+
     }
 }
