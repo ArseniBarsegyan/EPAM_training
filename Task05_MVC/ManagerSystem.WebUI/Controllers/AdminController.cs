ef1c3104b3fece1ea6067124c3a3723ebd11be75
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:55:25 +0300

    WebUI: AdminController: added Edit methods (get and post) redirect to another view where admin can edit user's data

--- Task05_MVC/ManagerSystem.WebUI/Controllers/AdminController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AdminController.cs
@@ -19,5 +19,29 @@ namespace ManagerSystem.WebUI.Controllers
         {
             return View(UserService.GetAllUsersList());
         }
+
+        public ActionResult Edit(string id)
+        {
+            var userDto = UserService.GetById(id);
+            if (userDto != null)
+            {
+                return View(userDto);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(UserDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var operationDetails = await UserService.EditAsync(userDto);
+
+                if (operationDetails.Succedeed)
+                    return RedirectToAction("Index", "Admin");
+                ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View();
+        }
     }
 }
