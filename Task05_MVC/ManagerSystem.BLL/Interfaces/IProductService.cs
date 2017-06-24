a91c57046d460b9bf855fc8cbff81a400f3835ab
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 11:43:00 +0300

    BLL: Interfaces: Added IProductService

---
+++ Task05_MVC/ManagerSystem.BLL/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ManagerSystem.BLL.DTO;
+using ManagerSystem.BLL.Infrastructure;
+
+namespace ManagerSystem.BLL.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductDto> GetAllProductsList();
+        ProductDto GetProductById(int id);
+        OperationDetails Create(ProductDto productDto);
+        OperationDetails Edit(ProductDto productDto);
+        OperationDetails Delete(int id);
+    }
+}
