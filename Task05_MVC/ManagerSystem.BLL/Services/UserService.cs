618ffa0b5213a3034f86c9d70081780ff42a6eee
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:11:01 +0300

    BLL: Services: UserService: Implemented GetById(string id). Added packages: Identity.Core, Identity.EF

--- Task05_MVC/ManagerSystem.BLL/Services/UserService.cs
+++ Task05_MVC/ManagerSystem.BLL/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using ManagerSystem.BLL.Interfaces;
+﻿using System.Linq;
+using ManagerSystem.BLL.DTO;
+using ManagerSystem.BLL.Interfaces;
 using ManagerSystem.DAL.Interfaces;
 
 namespace ManagerSystem.BLL.Services
@@ -10,6 +12,17 @@ namespace ManagerSystem.BLL.Services
         public UserService(IUnitOfWork unitOfWork)
         {
             UnitOfWork = unitOfWork;
+        }
+
+        public UserDto GetById(string id)
+        {
+            var user = UnitOfWork.UserManager.Users.FirstOrDefault(x => x.Id == id);
+            var userDto = new UserDto
+            {
+                Id = user.Id,
+                Name = user.UserName
+            };
+            return userDto;
         }
     }
 }
