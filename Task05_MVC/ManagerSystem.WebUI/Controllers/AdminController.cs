455d1f7de282036a96eec4ddd40d5b24de6cfa98
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:47:03 +0300

    WebUI: Controllers: added AdminController (access only for admin role)

---
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerSystem.WebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        
+    }
+}
