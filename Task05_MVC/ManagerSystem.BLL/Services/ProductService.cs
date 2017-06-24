89e27ace9d4245d8db38e4b9aaee3edc40a072b1
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:04:11 +0300

    BLL: Services: ProductService: Implemented GetAllProductsList() and GetProductById(int id)

--- Task05_MVC/ManagerSystem.BLL/Services/ProductService.cs
+++ Task05_MVC/ManagerSystem.BLL/Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using ManagerSystem.BLL.Interfaces;
+﻿using System.Collections.Generic;
+using ManagerSystem.BLL.DTO;
+using ManagerSystem.BLL.Interfaces;
 using ManagerSystem.DAL.Repositories;
 
 namespace ManagerSystem.BLL.Services
@@ -10,6 +12,35 @@ namespace ManagerSystem.BLL.Services
         public ProductService(UnitOfWork unitOfWork)
         {
             UnitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<ProductDto> GetAllProductsList()
+        {
+            var productList = new List<ProductDto>();
+
+            foreach (var product in UnitOfWork.ProductRepository.GetAll())
+            {
+                var productDto = new ProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price
+                };
+                productList.Add(productDto);
+            }
+            return productList;
+        }
+
+        public ProductDto GetProductById(int id)
+        {
+            var product = UnitOfWork.ProductRepository.GetById(id);
+            var productDto = new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price
+            };
+            return productDto;
         }
     }
 }
