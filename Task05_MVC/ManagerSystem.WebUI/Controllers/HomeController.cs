0d0fe049bbed9b55112ef5fe63d69fb3a20cec02
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 14:33:39 +0300

    WebUI: HomeController: finished Index() method - returns list of orders filtered by parameters and organized in pages

--- Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using System.Web.Mvc;
 using ManagerSystem.BLL.Interfaces;
 using ManagerSystem.BLL.Services;
 using ManagerSystem.DAL.Repositories;
+using ManagerSystem.WebUI.Models;
 
 namespace ManagerSystem.WebUI.Controllers
 {
@@ -15,7 +17,56 @@ namespace ManagerSystem.WebUI.Controllers
         public ActionResult Index(string manager, string product, string date, decimal? fromValue
             , decimal? toValue, int page = 1)
         {
-            return View();
+            var pageSize = 10;
+            var elementsPerPage = _orderService.GetAllOrderList().Skip((page - 1) * pageSize).Take(pageSize);
+            var pageInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = elementsPerPage.Count()
+            };
+            var orders = elementsPerPage;
+
+            var managers = orders.Select(x => x.ManagerName).Distinct().ToList();
+            managers.Insert(0, "All");
+            var products = orders.Select(x => x.ProductName).Distinct().ToList();
+            products.Insert(0, "All");
+            var dates = orders.Select(x => x.Date.ToString("d")).Distinct().ToList();
+            dates.Insert(0, "All");
+
+            var prices = orders.Select(x => x.Price).Distinct().ToList();
+            prices.Insert(0, 0m);
+
+            if (!string.IsNullOrEmpty(manager) && !manager.Equals("All"))
+            {
+                orders = orders.Where(x => x.ManagerName == manager);
+            }
+
+            if (!string.IsNullOrEmpty(product) && !product.Equals("All"))
+            {
+                orders = orders.Where(x => x.ProductName == product);
+            }
+
+            if (!string.IsNullOrEmpty(date) && !date.Equals("All"))
+            {
+                orders = orders.Where(x => x.Date.ToString("d") == date);
+            }
+
+            if (fromValue != null && fromValue != 0)
+            {
+                orders = orders.Where(x => x.Price >= fromValue && x.Price <= toValue);
+            }
+
+            var ordersListViewModel = new OrderListViewModel
+            {
+                Orders = orders,
+                Managers = new SelectList(managers),
+                Products = new SelectList(products),
+                Dates = new SelectList(dates),
+                PagingInfo = pageInfo
+            };
+
+            return View(ordersListViewModel);
         }
     }
 }
