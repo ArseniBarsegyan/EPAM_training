9c5cf6d8683c1724b9339aee2eb2385b51333b32
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 14:24:43 +0300

    WebUI: Models: added OrderListViewModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/OrderListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ManagerSystem.BLL.DTO;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class OrderListViewModel
+    {
+        public IEnumerable<OrderDto> Orders { get; set; }
+        public SelectList Managers { get; set; }
+        public SelectList Products { get; set; }
+        public SelectList Dates { get; set; }
+        public decimal FromValue { get; set; }
+        public decimal ToValue { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
