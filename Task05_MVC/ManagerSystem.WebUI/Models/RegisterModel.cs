490b73e2edff6e9e5ef4641cddd83238db02a281
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:28:55 +0300

    WebUI: Models: added RegisterModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
