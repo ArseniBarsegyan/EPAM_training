0378b70055acf13d876c134a294d7692ea269822
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:08:00 +0300

    BLL: Services: Added UserService: IUserService

---
+++ Task05_MVC/ManagerSystem.BLL/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.DAL.Interfaces;
+
+namespace ManagerSystem.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private IUnitOfWork UnitOfWork { get; }
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
