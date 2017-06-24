9c9a83c769f1818c8fc9e6056b5f79987a079fc0
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 13:22:08 +0300

    WebUI: ManagerController: added Edit() method (get and post)

--- Task05_MVC/ManagerSystem.WebUI/Controllers/ManagerController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/ManagerController.cs
@@ -34,5 +34,28 @@ namespace ManagerSystem.WebUI.Controllers
             }
             return View(managerDto);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var managerDto = _managerService.GetManagerById(id);
+            if (managerDto != null)
+            {
+                return View(managerDto);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ManagerDto managerDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var operationDetails = _managerService.Edit(managerDto);
+                if (operationDetails.Succedeed)
+                    return RedirectToAction("Index");
+                ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View(managerDto);
+        }
     }
 }
