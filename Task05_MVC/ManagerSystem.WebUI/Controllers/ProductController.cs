3120ffc28f93110f4316310d2d36c8e3c2408d5a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 13:26:48 +0300

    WebUI: Controllers: added ProductController: added field _productService, added Index() method (returns list of all products from DB)

---
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using ManagerSystem.BLL.Services;
+using ManagerSystem.DAL.Repositories;
+
+namespace ManagerSystem.WebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class ProductController : Controller
+    {
+        private IProductService _productService = new ProductService(new UnitOfWork("DefaultConnection"));
+
+        public ActionResult Index()
+        {
+            var allProducts = _productService.GetAllProductsList();
+            return View(allProducts);
+        }
+    }
+}
