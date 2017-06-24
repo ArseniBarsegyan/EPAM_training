bb7c5a9de2025f5aaaf60a440ecfb799d487ba7c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-24 12:34:30 +0300

    WebUI: AccountController: added fields UserService and AuthenticationManager (get from OWIN context)

--- Task05_MVC/ManagerSystem.WebUI/Controllers/AccountController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/AccountController.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
+using ManagerSystem.BLL.Interfaces;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace ManagerSystem.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
     }
 }
