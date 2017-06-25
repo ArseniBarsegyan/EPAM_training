0c3aa48e56ebf3df48bd3e1a1d010d229efc3244
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-25 16:30:51 +0300

    DAL: Entities: Order: added prop. TotalPrice

--- Task05_MVC/ManagerSystem.DAL/Entities/Order.cs
+++ Task05_MVC/ManagerSystem.DAL/Entities/Order.cs
@@ -6,6 +6,7 @@ namespace ManagerSystem.DAL.Entities
     {
         public DateTime Date { get; set; }
         public int ManagerId { get; set; }
+        public decimal TotalPrice { get; set; }
         public Manager Manager { get; set; }
         public Client Client { get; set; }
         public Product Product { get; set; }
