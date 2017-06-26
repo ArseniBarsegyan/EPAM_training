255a2e7cbbdb8931558a72ebdf824d39d3c77173
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-26 12:22:46 +0300

    WebUI: HomeController: now use PageList.MVC

--- Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
+++ Task05_MVC/ManagerSystem.WebUI/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using ManagerSystem.BLL.DTO;
 using ManagerSystem.BLL.Interfaces;
 using ManagerSystem.WebUI.Models;
+using ManagerSystem.WebUI.Util;
+using PagedList;
 
 namespace ManagerSystem.WebUI.Controllers
 {
@@ -11,7 +13,6 @@ namespace ManagerSystem.WebUI.Controllers
     {
         private IOrderService _orderService;
         private IManagerService _managerService;
-        private int PageSize = Convert.ToInt32(ConfigurationManager.AppSettings["PageSize"]);
 
         public HomeController(IOrderService orderService, IManagerService managerService)
         {
@@ -20,39 +21,33 @@ namespace ManagerSystem.WebUI.Controllers
         }
 
         public ActionResult Index(string manager, string product, string date, decimal? fromValue
-            , decimal? toValue, int page = 1)
+            , decimal? toValue, int? page)
         {
-            var pageSize = 10;
-            var elementsPerPage = _orderService.GetAllOrderList().Skip((page - 1) * pageSize).Take(pageSize);
-            var pageInfo = new PagingInfo
-            {
-                CurrentPage = page,
-                ItemsPerPage = PageSize,
-                TotalItems = elementsPerPage.Count()
-            };
-            var orders = elementsPerPage;
+            var pageSize = ConstantStorage.pageSize;
+            int pageNumber = (page ?? 1);
+            var orders = _orderService.GetAllOrderList();
 
             var managers = orders.Select(x => x.ManagerName).Distinct().ToList();
-            managers.Insert(0, "All");
+            managers.Insert(0, ConstantStorage.AllRecordsInListValue);
             var products = orders.Select(x => x.ProductName).Distinct().ToList();
-            products.Insert(0, "All");
+            products.Insert(0, ConstantStorage.AllRecordsInListValue);
             var dates = orders.Select(x => x.Date.ToString("d")).Distinct().ToList();
-            dates.Insert(0, "All");
+            dates.Insert(0, ConstantStorage.AllRecordsInListValue);
 
             var prices = orders.Select(x => x.Price).Distinct().ToList();
             prices.Insert(0, 0m);
 
-            if (!string.IsNullOrEmpty(manager) && !manager.Equals("All"))
+            if (!string.IsNullOrEmpty(manager) && !manager.Equals(ConstantStorage.AllRecordsInListValue))
             {
                 orders = orders.Where(x => x.ManagerName == manager);
             }
 
-            if (!string.IsNullOrEmpty(product) && !product.Equals("All"))
+            if (!string.IsNullOrEmpty(product) && !product.Equals(ConstantStorage.AllRecordsInListValue))
             {
                 orders = orders.Where(x => x.ProductName == product);
             }
 
-            if (!string.IsNullOrEmpty(date) && !date.Equals("All"))
+            if (!string.IsNullOrEmpty(date) && !date.Equals(ConstantStorage.AllRecordsInListValue))
             {
                 orders = orders.Where(x => x.Date.ToString("d") == date);
             }
@@ -64,14 +59,21 @@ namespace ManagerSystem.WebUI.Controllers
 
             var ordersListViewModel = new OrderListViewModel
             {
-                Orders = orders,
+                Orders = orders.ToPagedList(pageNumber, pageSize),
                 Managers = new SelectList(managers),
                 Products = new SelectList(products),
                 Dates = new SelectList(dates),
-                PagingInfo = pageInfo
             };
 
             return View(ordersListViewModel);
+        }
+
+        public ActionResult OrdersSummary(IEnumerable<OrderDto> orderDtos, int? page)
+        {
+            var pageSize = ConstantStorage.pageSize;
+            int pageNumber = (page ?? 1);
+
+            return PartialView(orderDtos.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult GetManagersData()
