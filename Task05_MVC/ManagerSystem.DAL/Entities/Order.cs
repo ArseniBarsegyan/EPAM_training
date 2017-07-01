e33a0e8b702c38915f5fb08b713b5d6d1b4ed616
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-01 10:00:56 +0300

    DAL: Corrected Order entity

--- Task05_MVC/ManagerSystem.DAL/Entities/Order.cs
+++ Task05_MVC/ManagerSystem.DAL/Entities/Order.cs
@@ -8,7 +8,9 @@ namespace ManagerSystem.DAL.Entities
         public int ManagerId { get; set; }
         public decimal TotalPrice { get; set; }
         public Manager Manager { get; set; }
+        public int ClientId { get; set; }
         public Client Client { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
     }
 }
