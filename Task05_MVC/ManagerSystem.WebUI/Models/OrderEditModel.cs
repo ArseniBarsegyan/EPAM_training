2dcd4f49143054b6bac452af501636fcf2a1126e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 18:19:18 +0300

    WebUI: added OrderEditModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/OrderEditModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class OrderEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z\s]+", ErrorMessage = "Name can't contain numbers")]
+        public string ManagerName { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z\s]+", ErrorMessage = "Name can't contain numbers")]
+        public string ClientName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
