e7efe8b0cb29e989fcd5efce734fc64f44ab3449
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 17:50:11 +0300

    WebUI: ProductCreateModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/ProductCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class ProductCreateModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
