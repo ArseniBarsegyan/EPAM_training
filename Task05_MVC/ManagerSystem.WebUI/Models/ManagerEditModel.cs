85857f020d8747b652f14358f9f1487c67c737ee
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 18:19:38 +0300

    WebUI: added ManagerEditModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/ManagerEditModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class ManagerEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z\s]+", ErrorMessage = "Name can't contain numbers")]
+        public string LastName { get; set; }
+    }
+}
