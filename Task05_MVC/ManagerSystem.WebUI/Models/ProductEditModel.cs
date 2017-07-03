775160479b47918758e6f77c6837fe7c76b5434f
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 18:18:56 +0300

    WebUI: added ProductEditModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/ProductEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class ProductEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z\s]+", ErrorMessage = "Name can't contain numbers")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
