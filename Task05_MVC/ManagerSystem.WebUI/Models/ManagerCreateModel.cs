ec3ec69e72a0e47ab5c8211cfcc964f95240dfda
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 17:49:50 +0300

    WebUI: Added ManagerCreateModel

---
+++ Task05_MVC/ManagerSystem.WebUI/Models/ManagerCreateModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerSystem.WebUI.Models
+{
+    public class ManagerCreateModel
+    {
+        [Required]
+        [RegularExpression(@"[A-Za-z\s]+", ErrorMessage = "Name can't contain numbers")]
+        public string LastName { get; set; }
+    }
+}
