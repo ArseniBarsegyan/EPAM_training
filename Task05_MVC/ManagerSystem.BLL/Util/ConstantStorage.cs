f1370440c92d38547b2ce97cf7785f2d5a1c7b92
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-03 15:05:06 +0300

    BLL: Added class ConstantStorage (stores string values)

---
+++ Task05_MVC/ManagerSystem.BLL/Util/ConstantStorage.cs
@@ -0,0 +1,15 @@
+﻿namespace ManagerSystem.BLL.Util
+{
+    public static class ConstantStorage
+    {
+        public const string ManagerCreated = "manager create successful";
+        public const string ManagerUpdated = "manager update successful";
+        public const string ManagerDeleted = "manager update successful";
+        public const string OrderCreated = "order create successful";
+        public const string OrderUpdated = "order update successful";
+        public const string OrderDeleted = "order delete successful";
+        public const string ProductCreated = "product create successful";
+        public const string ProductUpdated = "product update successful";
+        public const string ProductDeleted = "product delete successful";
+    }
+}
